517254cdb35456ce49858d1a38332ded7a3de3b2
VladyslavHn <vlad.gnatok@gmail.com>
2024-08-12 14:45:16 +0200

    dynamic_route

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Sidebar from "@/app/components/sidebar";
+
+export interface LayoutProps {
+    children: React.ReactNode;
+};
+
+export default function Layout({children}: LayoutProps) {
+    return (
+        <>
+            <Sidebar/>
+            <div className="ml-60">{children}</div>
+        </>
+    );
+}
